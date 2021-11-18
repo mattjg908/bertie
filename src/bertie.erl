67723e52748bf598a07fb1d631603a3b29423e57
Matt Galvin <matthewgalvin908@gmail.com>
2021-11-17 20:01:27 -0500

    Add bitcask

--- src/bertie.erl
+++ src/bertie.erl
@@ -1,4 +1,18 @@
 -module(bertie).
 -export([start/0]).
+      
+start() ->
+    Handle = bitcask:open("bertie_database", [read_write]),
+    N = fetch(Handle),
+    store(Handle, N+1),
+    io:format("Bertie has been run ~p times~n",[N]),
+    bitcask:close(Handle),
+    init:stop().
 
-start() -> io:format("Hello my name is Bertie~n").
+store(Handle, N) ->
+    bitcask:put(Handle, <<"bertie_executions">>, term_to_binary(N)).  
+fetch(Handle) ->
+    case bitcask:get(Handle, <<"bertie_executions">>) of
+	not_found -> 1;
+	{ok, Bin} -> binary_to_term(Bin)
+    end.
